6de8204bd8b3a68d6345f1bbbc66922837b16b90
nrsmith075 <126043130+nrsmith075@users.noreply.github.com>
2023-05-10 15:25:42 +1000

    NS - Changes to Product Update and Delete

--- src/java/DAO/DB.java
+++ src/java/DAO/DB.java
@@ -15,5 +15,4 @@ public abstract class DB {
     protected String dbpass = "password"; //db root password   
     protected String driver = "org.apache.derby.jdbc.ClientDriver"; //jdbc client driver - built in with NetBeans   
     protected Connection conn; //connection null-instance to be initialized in sub-classes
-
 }
