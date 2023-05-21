6a5642b6228a7da3d4dde9debca424f1d94381c3
DewanStrydom <dewanstrydom@gmail.com>
2023-05-21 20:22:52 +1000

    yes

---
+++ web/SQL db/OrderSQL.sql
@@ -0,0 +1,17 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/SQLTemplate.sql to edit this template
+ */
+/**
+ * Author:  Dewan Laptop
+ * Created: 21/05/2023
+ */
+CREATE TABLE ORDERS
+(
+ORDERID int,
+PRODUCTNAME varchar(100),
+PRODUCT_ID int,
+PRICE double,
+PRIMARY KEY(ORDERID)
+
+);
---
+++ web/SQL db/OrderSQL_data.sql
@@ -0,0 +1,31 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/SQLTemplate.sql to edit this template
+ */
+/**
+ * Author:  Dewan Laptop
+ * Created: 21/05/2023
+ */
+
+INSERT INTO STUDENTS (ORDERID, PRODUCTNAME, PRODUCT_ID, PRICE)
+VALUES
+(1000, Smartphone,100,,),
+(1001, Laptop,101,),
+(1002, Tablet,102,),
+(1003, Smartwatch,103,),
+(1004, Smart TV,104,),
+(1005, Gaming console,105,),
+(1006, Virtual reality headset,106,),
+(1007, Digital camera,107,),
+(1008, Fitness tracker,108,),
+(1009, Bluetooth speaker,109,),
+(1010, Wireless headphones,110,),
+(1011, Portable power bank,111,),
+(1012, E-book reader,112,),
+(1013, Google Home,113,),
+(1014, Drone,114,),
+(1015, Smart thermostat,115,),
+(1016, Wireless router,116,),
+(1017, Smart doorbell,117,),
+(1018, Smart lock,118,),
+(1019, Portable Bluetooth printer,119,);
